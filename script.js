b1bf83cac852221ff50c83c7da1f635c540f652a
UsmaanShafiq <usmanshafiq933@gmail.com>
2022-10-24 03:11:56 +0500

    modalbox and js work in process

---
+++ script.js
@@ -0,0 +1,56 @@
+//Selectors
+const addModalBtn = document.getElementById('modal-btn');
+const modal = document.getElementById('modal-box-container');
+const closeModalBtn = document.getElementById('close-modal');
+const bookForm = document.getElementById('addBookForm');
+
+
+//Show Modal Box
+addModalBtn.addEventListener("click", () => {
+    modal.style.visibility = "visible";
+});
+
+closeModalBtn.addEventListener('click', () => {
+    modal.style.visibility = "hidden";
+    bookForm.reset(); // reset form on close modal
+});
+
+
+let myLibrary = [];
+
+
+function Book(e){
+    e.preventDefault(); // Prevent refresh after form submission
+    const myFormData = new FormData(e.target);
+    
+    //const formDataObj = {};
+    //myFormData.forEach((value, key) => (formDataObj[key] = value));
+
+    const formDataObj = Object.fromEntries(myFormData.entries());
+    myLibrary.push(formDataObj);
+    
+    
+    myLibrary.forEach((val) =>{
+        console.log(val);
+        document.getElementById('bookCardContainer').innerHTML = `
+        <div class="card">
+            <p class="book-name">${val.book_name}</p>
+            <p class="book-author">${val.book_author}</p>
+            <p class="book-pages">${val.book_pages}</p>
+            <div class="btn-group">
+                <input type="image" src="./assets/icons/eye.svg" class="read-btn" alt="read_btn">
+                <input type="image" src="./assets/icons/delete.svg" class="del-btn" alt="delete_btn">
+            </div>
+        </div>    
+        `;
+    });
+    
+    
+    
+    console.log(myLibrary);
+    bookForm.reset(); // reset form after submit
+    modal.style.visibility = "hidden"; // hide modal after sumit
+}
+
+//Getting Form Data
+bookForm.addEventListener('submit' ,  Book);
