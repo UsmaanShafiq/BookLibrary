a1b191c89bc9567891af688fce285a71f203966d
UsmaanShafiq <usmanshafiq933@gmail.com>
2022-10-26 10:28:09 +0500

    delete book feature in process

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const addModalBtn = document.getElementById('modal-btn');
 const modal = document.getElementById('modal-box-container');
 const closeModalBtn = document.getElementById('close-modal');
 const bookForm = document.getElementById('addBookForm');
+const cardContainer= document.getElementById("bookCardContainer");
+const cards = document.getElementsByClassName('card');
+const bookReadBtn = document.getElementsByClassName('read-btn');
+const bookDelBtns = document.getElementsByClassName('del-btn');
+
+
 
 
 //Show Modal Box
@@ -30,32 +36,59 @@ function Book(e){
     myLibrary.push(formDataObj);
     
     
-    const card = myLibrary.map((val) =>{
+    const card = myLibrary.map((val, index) =>{
         return `
         <div class="card">
             <p class="book-name">${val.book_name}</p>
             <p class="book-author">${val.book_author}</p>
             <p class="book-pages">${val.book_pages}</p>
             <div class="btn-group">
-                <input type="image" src="./assets/icons/eye.svg" class="read-btn" alt="read_btn">
-                <input type="image" src="./assets/icons/delete.svg" class="del-btn" alt="delete_btn">
+                <input type="image" 
+                    src="./assets/icons/eye.svg" 
+                    class="read-btn" data-id="${index}" 
+                    alt="read_btn"
+                >
+                <input 
+                    type="image" 
+                    src="./assets/icons/delete.svg" 
+                    class="del-btn" data-id="${index}" 
+                    alt="delete_btn"
+                >
             </div>
         </div>    
         `
     }).join('');
-    document.getElementById("bookCardContainer").innerHTML= card; //render card into the seleted element
+    cardContainer.innerHTML= card; //render card into the seleted element
 
 
-        if (myLibrary.length > 0) {   
-            const bookDelBtn = document.querySelectorAll('.del-btn');
-            console.log(bookDelBtn);
-        }
-    
-    
+    if (cards.length>0) {
+        Array.from(bookDelBtns).map(delBtn => {
+            delBtn.addEventListener('click', delBook);
+        });   
+    }
+
+    // Modal Box
     bookForm.reset(); // reset form after submit
     modal.style.visibility = "hidden"; // hide modal after sumit
 }
-
-
 //Getting Form Data
 bookForm.addEventListener('submit' ,  Book);
+
+
+function delBook(e) {
+    const cardID = e.target.getAttribute('data-id');
+    console.log(cardID);
+    }
+
+
+if (cards.length>0) {
+    Array.from(bookDelBtns).map(delBtn => {
+        delBtn.addEventListener('click', delBook);
+    });   
+}else{
+    modal.style.visibility = "visible"; // show modal box
+}
+
+
+
+
